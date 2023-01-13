2c20775c42ef423cbdfe04392bac0ffa4b11c1a7
Razeen Shaikh <razeen9796@outlook.com>
2023-01-13 15:39:22 +0530

    removed comments

--- src/pages/TrendingAssets/index.tsx
+++ src/pages/TrendingAssets/index.tsx
@@ -29,68 +29,6 @@ const TrendingAssets = () => {
           />
         )
       )}
-      {/* <div className="card mx-auto flex justify-center">
-        <div className="outside-circle">
-          <div className="inside-circle flex justify-center items-center">
-            <div className="icon-circle flex justify-center items-center">
-              <img src={Bitcoin} alt="Bitcoin" />
-            </div>
-          </div>
-        </div>
-        <p>Shiba Inu (SHIB)</p>
-        <p>$0.00000001948</p>
-        <p>Price</p>
-        <p>$60,000</p>
-        <p>tvl</p>
-        <div className="tvl">
-          <div className="solana">
-            <img src={Solana} alt="" />
-          </div>
-          <div className="ethereum">
-            <img src={Ethereum} alt="" />
-          </div>
-          <div className="binance">
-            <img src={Binance} alt="" />
-          </div>
-        </div>
-        <p>popular pairs</p>
-      </div>
-      <div className="card mx-auto flex justify-center">
-        <div className="outside-circle">
-          <div className="inside-circle flex justify-center items-center">
-            <div className="icon-circle flex justify-center items-center">
-              <img src={Solana} alt="Bitcoin" />
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="card mx-auto flex justify-center">
-        <div className="outside-circle">
-          <div className="inside-circle flex justify-center items-center">
-            <div className="icon-circle flex justify-center items-center">
-              <img src={Ethereum} alt="Bitcoin" />
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="card mx-auto flex justify-center">
-        <div className="outside-circle">
-          <div className="inside-circle flex justify-center items-center">
-            <div className="icon-circle flex justify-center items-center">
-              <img src={Binance} alt="Bitcoin" />
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="card mx-auto flex justify-center">
-        <div className="outside-circle">
-          <div className="inside-circle flex justify-center items-center">
-            <div className="icon-circle flex justify-center items-center">
-              <img src={SHIBA} alt="Bitcoin" />
-            </div>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 };
